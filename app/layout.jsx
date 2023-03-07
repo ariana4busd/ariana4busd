13af2f5b9c996be76ac9c66a2359ca0ea8ba53f4
Tony <tony@onions.dev>
2023-03-06 23:09:34 -0800

    undo new tab for donate page

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,11 +19,7 @@ export default function RootLayout({ children }) {
             </Link>
             <Link href="/about">About</Link>
             <Link href="/vote">Get Involved</Link>
-            <Link
-              href="https://secure.fundhero.com/ariana-martinez-for-benicia-school-board-2023"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <Link href="https://secure.fundhero.com/ariana-martinez-for-benicia-school-board-2023">
               Donate
             </Link>
           </nav>
