c262e275281886a9ba2021f95ef73115da86b1e0
Tony <tony@onions.dev>
2023-03-07 20:06:13 -0800

    update voting page style

--- app/(support)/vote/page.jsx
+++ app/(support)/vote/page.jsx
@@ -6,32 +6,27 @@ import { Icon } from "@app/icon";
 export default function Vote() {
   return (
     <section id="vote" className={css.section}>
-      <h1>Vote</h1>
-      <div className={css.guide}>
+      <div className={css.vote}>
         <p>
           March 27th is is the last day to register or update your voter
-          registration in this special election
+          registration in this special election. Only those who live in Trustee
+          Area 5 are able to vote in this special election. Please see the map
+          for Trustee Area 5 and the polling location.
         </p>
 
-        <p>
-          Only those who live in Trustee Area 5 are able to vote in this special
-          election
-        </p>
-
-        <p>Please see the map for Trustee Area 5 and the polling location</p>
+        <Link
+          className={css.register}
+          href="https://registertovote.ca.gov/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/vote.png" alt="" width={64} height={64} />
+          Register to Vote
+          <Icon id="ic_ext_link" />
+        </Link>
       </div>
 
-      <Link
-        className={css.register}
-        href="https://registertovote.ca.gov/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image src="/vote.png" alt="" width={64} height={64} />
-        Register to Vote
-        <Icon id="ic_ext_link" />
-      </Link>
-
+      {/* TODO embed interactive map */}
       <picture className={css.district_map}>
         <Image src="/district_area_5.png" alt="Trustee Area 5" fill />
       </picture>
